240068cb25a89e285d4877b927fa8579ba863e34
JaroslavBes <besja00@yandex.ru>
2020-10-16 22:28:55 +0300

    clone_min.sh: Скрипт для получения минимального набора исходников

---
+++ clone_min.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/pgul/binkd.git
+git clone https://github.com/golded-plus/golded-plus.git
+git clone https://github.com/huskyproject/huskybse.git
+git clone https://github.com/huskyproject/huskylib.git
+git clone https://github.com/huskyproject/smapi.git
+git clone https://github.com/huskyproject/fidoconf.git
+git clone https://github.com/huskyproject/areafix.git
+git clone https://github.com/huskyproject/hpt.git
+git clone https://github.com/huskyproject/htick.git
+git clone https://github.com/huskyproject/sqpack.git
